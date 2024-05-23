1daae52bce4185c2a17a5bb501b079935b8ed4b0
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-05-23 21:27:53 +0200

    add types file

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export interface Movie {
+  actors: string[]
+  desc: string
+  directors: string[]
+  genre: string[]
+  image_url: string
+  thumb_url: string
+  imdb_url: string
+  name: string
+  rating: number
+  year: number
+}
