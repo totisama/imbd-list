144e40bfd47142ba8989bee0f6c6842486b13a9f
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-05-26 19:31:53 +0200

    Add filters component and feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,30 @@
 import { type Movie } from '@/types'
 import { MovieCard } from '@/components/MovieCard'
+import { Filters } from '@/components/Filters'
 
-export default async function Home () {
-  const movies: Movie[] = await fetch('https://raw.githubusercontent.com/theapache64/top250/master/top250_min.json?authuser=1')
-    .then(async (res) => await res.json())
+export default async function Home ({ searchParams }: { searchParams: { sort?: string, name?: string } }) {
+  let movies: Movie[] = await fetch(
+    'https://raw.githubusercontent.com/theapache64/top250/master/top250_min.json?authuser=1'
+  ).then(async (res) => await res.json())
+  const { sort, name } = searchParams
+
+  if (sort === 'true') {
+    movies.sort((a, b) => a.rating - b.rating)
+  }
+
+  if (name !== undefined && name !== '') {
+    movies = movies.filter((movie) => movie.name.toLocaleLowerCase().startsWith(name.toLocaleLowerCase()))
+    console.log(movies)
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center py-20">
       <h1 className="text-5xl text-green-500">IMDB 250 List</h1>
       <section className="w-4/5 mt-8">
+        <Filters searchParams={searchParams} />
         <ul className="flex justify-center flex-wrap gap-5">
-          {movies.map((movie) => (
-            <MovieCard key={movie.name} movie={movie} />
+          {movies.map((movie, index) => (
+            <MovieCard key={movie.name} movie={movie} index={index} />
           ))}
         </ul>
       </section>
