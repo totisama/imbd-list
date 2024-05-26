c3cfd2fe08c7f28727245e90de924036574ef544
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-05-26 19:32:12 +0200

    add index prop to set image priority

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -2,14 +2,16 @@ import { type Movie } from '@/types'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export const MovieCard = ({ movie }: { movie: Movie }) => {
+interface MovieCardProps { movie: Movie, index: number }
+
+export const MovieCard = ({ movie, index }: MovieCardProps) => {
   const { name, image_url: image, year, rating } = movie
   const href = `/movie/${name.replaceAll(' ', '-')}`
 
   return (
     <li className='group relative flex w-44 flex-col items-center rounded-md bg-gray-600 lg:w-60' key={name}>
       <Link className='py-3 max-w-44 lg:max-w-60 hover:cursor-pointer' href={href}>
-        <Image className='px-2' src={image} width={500} height={500} alt={`${name} image`} />
+        <Image className='px-2' priority={index <= 10 } src={image} width={500} height={500} alt={`${name} image`} />
         <div className='py-5 hidden absolute bottom-0 text-center transition-all ease-in-out duration-300 bg-gray-800/95 w-full group-hover:block'>
           <h2 className='text-xl'>{name}</h2>
           <div className='flex flex-col gap-0 justify-center lg:gap-5 lg:flex-row'>
