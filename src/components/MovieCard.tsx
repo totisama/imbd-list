0816180da0ccec37a091cbbdb1a1e857dd04b238
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-05-27 11:24:08 +0200

    Add motion li and handle image error

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,18 +1,37 @@
+'use client'
+
 import { CheckIcon } from '@/icons'
 import { type Movie } from '@/types'
 import Image from 'next/image'
 import Link from 'next/link'
+import { motion } from 'framer-motion'
+import { useState } from 'react'
 
 interface MovieCardProps { movie: Movie, index: number, seen: boolean }
 
 export const MovieCard = ({ movie, index, seen }: MovieCardProps) => {
   const { name, image_url: image, year, rating } = movie
-  const href = `/movie/${name.replaceAll(' ', '-')}`
+  const href = `/movie/${encodeURIComponent(name.replaceAll(' ', '-'))}`
+  const [error, setError] = useState(false)
 
   return (
-    <li className='group relative flex w-44 flex-col items-center rounded-md bg-gray-600 lg:w-60' key={name}>
+    <motion.li
+      key={name}
+      initial={{ x: -50, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{ duration: 0.75 }}
+      className='group relative flex w-44 flex-col items-center rounded-md bg-gray-600 lg:w-60'
+    >
       <Link className='py-3 max-w-44 lg:max-w-60 hover:cursor-pointer' href={href}>
-        <Image className='px-2' priority={index <= 10 } src={image} width={500} height={500} alt={`${name} image`} />
+        <Image
+          src={error ? '/notFound.png' : image}
+          alt={`${name} image`}
+          width={500}
+          height={500}
+          priority={index <= 10}
+          onError={(e) => { setError(true) }}
+          className='px-2'
+        />
         <div className='py-5 hidden absolute bottom-0 text-center transition-all ease-in-out duration-300 bg-gray-800/95 w-full group-hover:block'>
           {seen && <span className='absolute top-0 left-0'><CheckIcon className='w-8 h-8' /></span>}
           <h2 className='text-xl'>{name}</h2>
@@ -22,6 +41,6 @@ export const MovieCard = ({ movie, index, seen }: MovieCardProps) => {
           </div>
         </div>
       </Link>
-    </li>
+    </motion.li>
   )
 }
